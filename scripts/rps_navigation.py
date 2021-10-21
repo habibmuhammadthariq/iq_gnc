c42e29ba78dae5a42f76b76c7a0ab7f702457660
habibmuhammadthariq <thariqhabibmuhammad@gmail.com>
2021-10-21 10:44:24 +0700

    update rps_navigation.py file

--- scripts/rps_navigation.py
+++ scripts/rps_navigation.py
@@ -25,95 +25,95 @@ cap.set(cv2.CAP_PROP_FPS, 15)
 rate = rospy.Rate(3)
 
 def rps_detection(img):
-	#load model
-	model = keras.models.load_model('rock_paper_scissors_model')
+    #load model
+    model = keras.models.load_model('rock_paper_scissors_model')
 	
-	x = image.img_to_array(img)
-	x = np.expand_dims(x, axis=0)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
 
-	images = np.vstack([x])
-	classes = model.predict(images, batch_size=10)
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
 	
-	if classes[0,0]!=0:
-		print('paper')
-		result = 1
-	elif classes[0,1]!=0:
-		print('rock')
-		result = 2
-	else:
-		print('scissors')
-    	result = 3
+    if classes[0,0]!=0:
+        print('paper')
+        result = 1
+    elif classes[0,1]!=0:
+        print('rock')
+        result = 2
+    else:
+        print('scissors')
+        result = 3
 	
-	return (result)
+    return (result)
 
 def rock():
- 	#Specify some waypoints to
-	goals = [[0, 0, 3, 0], [5, 0, 3, -90], [5, 5, 3, 0],
+    #Specify some waypoints to
+    goals = [[0, 0, 3, 0], [5, 0, 3, -90], [5, 5, 3, 0],
              [0, 5, 3, 90], [0, 0, 3, 180], [0, 0, 3, 0]]
-	i = 0
+    i = 0
 
-	while i < len(goals):
-		drone.set_destination(
+    while i < len(goals):
+        drone.set_destination(
             x=goals[i][0], y=goals[i][1], z=goals[i][2], psi=goals[i][3])
-		rate.sleep()
-		if drone.check_waypoint_reached():
-			i += 1
+        rate.sleep()
+        if drone.check_waypoint_reached():
+            i += 1
 def paper():
-	#Specify some waypoints
-	goals = [[0, 0, 3, 0], [5, 0, 3, -40]]
-	i = 0
+    #Specify some waypoints
+    goals = [[0, 0, 3, 0], [5, 0, 3, -40]]
+    i = 0
 
-	while i < len(goals):
-		drone.set_destination(
+    while i < len(goals):
+        drone.set_destination(
             x=goals[i][0], y=goals[i][1], z=goals[i][2], psi=goals[i][3])
-		rate.sleep()
-		if drone.check_waypoint_reached():
-			i += 1
+        rate.sleep()
+        if drone.check_waypoint_reached():
+            i += 1
 	
     # Land after all waypoints is reached.
-	drone.land()
-	rospy.loginfo(CGREEN2 + "All waypoints reached landing now." + CEND)
+    drone.land()
+    rospy.loginfo(CGREEN2 + "All waypoints reached landing now." + CEND)
     
 def scissors():
-     drone.land()
-     rospy.loginfo(CGREEN2 + "Landing now." + CEND)
+    drone.land()
+    rospy.loginfo(CGREEN2 + "Landing now." + CEND)
      
 def main():
-	# Initializing the ROS node.
-	rospy.init_node("rps_navigation", anonymous=True)
+    # Initializing the ROS node.
+    rospy.init_node("rps_navigation", anonymous=True)
 
     # Create an object for the API.
-	drone = gnc_api()	
+    drone = gnc_api()	
     # Wait for FCU connection.
-	drone.wait4connect()
+    drone.wait4connect()
     # Wait for the mode to be switched.
-	drone.wait4start()
+    drone.wait4start()
     # Create local reference frame.
-	drone.initialize_local_frame()
+    drone.initialize_local_frame()
     # Request takeoff with an altitude of 2m.
-	drone.takeoff(3)
+    drone.takeoff(3)
 	
 	#image detectioin looping
-	while True :
-		ret, img = capt.read()
+    while True :
+        ret, img = capt.read()
 		
-		#hand gesture detection
-		hand_gesture = rps_detection(img)
-		if(hand_gesture==1):
-			paper()
-		elif(hand_gesture==2):
-			rock()
-		else:
-			scissors()
+	#hand gesture detection
+        hand_gesture = rps_detection(img)
+        if(hand_gesture==1):
+            paper()
+        elif(hand_gesture==2):
+            rock()
+        else:
+            scissors()
 		
     # Used to keep the node running.
-	rospy.spin()
+    rospy.spin()
 
 if __name__ == '__main__':
-	try:
-		main()
-	except KeyboardInterrupt:
-		exit()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
 	
 
 
