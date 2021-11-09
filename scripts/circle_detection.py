c91f36afb7d5a5548fd94e54fa815a245b49c627
habibmuhammadthariq <thariqhabibmuhammad@gmail.com>
2021-11-09 16:05:59 +0700

    ...

--- scripts/circle_detection.py
+++ scripts/circle_detection.py
@@ -6,46 +6,51 @@ import cv2
 import numpy as np
 import modul
 
+#note
+#knwon_width=19.5
+#focal_length=784.615384615
+
 def callback(data):
+    #bridge = CvBridge()
     try:
         cv_img = bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
         print (e)
 
     #start and find the object
-#    marker = modul.finding_object(cv_img)
- #   if len(marker) > 0:
+    marker = modul.finding_object(cv_img)
+    if len(marker) > 0:
         #get centroid of the object and frame
- #       cx_img, cy_img = modul.centroid_image_v2(marker)
- #       cx_frm, cy_frm = modul.centroid_frame()
- #       print "cX and cY image : {},{} - cX and cY frame : {},{}".format(cx_img,cy_img,cx_frm,cy_frm)
+        cx_img, cy_img = modul.centroid_image_v2(marker)
+        cx_frm, cy_frm = modul.centroid_frame()
+        print "cX and cY image : {},{} - cX and cY frame : {},{}".format(cx_img,cy_img,cx_frm,cy_frm)
     
         #draw line from centroid image into centroid frame
- #       cv2.line(modul.img, (cx_img,cy_img), (cx_frm,cy_frm), (255,255,255), 4)
+        cv2.line(modul.img, (cx_img,cy_img), (cx_frm,cy_frm), (255,255,255), 4)
 
         #finding distance from camera to image
- #       focal_length, known_width = modul.finding_focal_length()
- #       detected_marker = modul.detail_object(marker,"biggest_contour")
- #       distance = modul.distance_to_camera(known_width, focal_length, detected_marker[1][0])
+        detected_marker = modul.detail_object(marker,"biggest_contour")
+        #we can get focal_length using find_distance_from_camera_to_image.py file and of course we need to change any variable there
+        distance = modul.distance_to_camera(19.5, 784.615384615, detected_marker[1][0])
 
         #print out the result
-  #      cv2.putText(modul.img, "distanece   : {} cm".format(distance), (30,25), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,255,255), 2)
- #       cv2.putText(modul.img, "x direction : {} px".format(cx_frm-cx_img), (30,45), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,255,255), 2)
-  #      cv2.putText(modul.img, "y direction : {} px".format(cy_frm-cy_img), (30,65), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,255,255), 2)
+        cv2.putText(modul.img, "distanece   : {} cm".format(distance), (30,25), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,255,255), 2)
+        cv2.putText(modul.img, "x direction : {} px".format(cx_frm-cx_img), (30,45), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,255,255), 2)
+        cv2.putText(modul.img, "y direction : {} px".format(cy_frm-cy_img), (30,65), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,255,255), 2)
     
         #displaying the result in original image
-        cv2.imshow("Result", cv_img)#modul.img)
+        cv2.imshow("Result", modul.img)
         cv2.waitKey(1)
 
         try:
-            image_pub.Publish(bridge.cv2_to_imgmsg(cv_img, "bgr8"))
+            img_pub.publish(bridge.cv2_to_imgmsg(modul.img, "bgr8"))
         except CvBridgeError as e:
             print (e)
 
 def sub_pub():
     global img_pub
     global bridge
-    rospy.init_node("circle_detection", anonymous=True)
+    rospy.init_node("CircleDetection", anonymous=True)
     rospy.Subscriber("/webcam", Image, callback)
     img_pub = rospy.Publisher("/detected_object", Image, queue_size=1)
     bridge = CvBridge()	
@@ -56,4 +61,4 @@ if __name__ == '__main__':
         sub_pub()
     except cv2.ROSInterruptException:
         pass
-    modul.destroy()
+    #modul.destroy()
--- scripts/object_following.py
+++ scripts/object_following.py
@@ -12,56 +12,78 @@ from iq_gnc.py_gnc_functions import *
 from iq_gnc.PrintColours import *
 # Importing Point message from package geometry_msgs.
 from geometry_msgs.msg import Point
+#
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class detect_an_object:
+    def __init__(self):
+        self.contours =  [] #this is list
+        self.bridge = CvBridge()
+        self.img_sub = rospy.Subscriber("/webcam", Image, 1, self.img_cb) #10 -> queue_size
+
+    def img_cb(self, data):
+        try:
+            cv_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except rospy.ROSInterruptException as e:
+            print (e)
+     
+        self.contours = modul.finding_object(cv_img)
+
+    def get_ojbect(self):
+        return self.contours
+
+    def get_centroid_object(self):
+        next = True
+
+        if len(self.contours) > 0:
+            cx_obj, cy_obj = modul.centroid_image_v2(self.contours)
+            cx_frm, cy_frm = modul.centroid_frame()
+            print "cX and cY object : {},{} - cX and cY frame : {},{}".format(cx_obj,cy_obj,cx_frm,cy_frm)
+
+            #draw line from centroid image into centroid object
+            cv2.line(modul.img, (cx_obj,cy_obj), (cx_frm,cy_frm), (255,255,255), 4)
+
+            if ((cx_frm-cx_obj) < 0) and ((cy_frm-cy_obj) < 0):
+                x = -0.5
+                y = -0.5
+            elif ((cx_frm-cx_obj) < 0) and ((cy_frm-cy_obj) > 0):
+                x = -0.5
+                y = 0.5
+            elif ((cx_frm-cx_obj) > 0) and ((cy_frm-cy_obj) < 0):
+                x = 0.5
+                y = -0.5
+            elif ((cx_frm-cx_obj) > 0) and ((cy_frm-cy_obj) > 0):
+                x = 0.5
+                y = 0.5
+            else:
+#            x = x
+#            y = y
+                next = False
+
+            return x,y,next
+
+
+    def get_object_distance(self):
+        if len(self.contours) > 0:
+            #finding distance from camera into image
+            marker = modul.detail_object(self.contours, "biggest_contour")
+            #we can get focal_length using find_distance_from_camera_to_image.py file and of course we need to change any variable there
+            distance = modul.distance_to_camera(19.5, 784.615384615, marker[1][0])
+
+            #print out the result
+            cv2.putText(modul.img, "Distance camera to object : ".format(distance), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,255,255), 2)
+
+            return distance
+
+
 
 # Create an object for the API and make it as global variable
 drone = gnc_api()
 
-def get_centroid_object(marker):
-    next = True
-
-    if len(marker) > 0:
-        cx_obj, cy_obj = modul.centroid_image_v2(marker)
-        cx_frm, cy_frm = modul.centroid_frame()
-        print "cX and cY object : {},{} - cX and cY frame : {},{}".format(cx_obj,cy_obj,cx_frm,cy_frm)
-
-        #draw line from centroid image into centroid object
-        cv2.line(modul.img, (cx_obj,cy_obj), (cx_frm,cy_frm), (255,255,255), 4)
-
-        if ((cx_frm-cx_obj) < 0) and ((cy_frm-cy_obj) < 0):
-            x = -0.5
-            y = -0.5
-        elif ((cx_frm-cx_obj) < 0) and ((cy_frm-cy_obj) > 0):
-            x = -0.5
-            y = 0.5
-        elif ((cx_frm-cx_obj) > 0) and ((cy_frm-cy_obj) < 0):
-            x = 0.5
-            y = -0.5
-        elif ((cx_frm-cx_obj) > 0) and ((cy_frm-cy_obj) > 0):
-            x = 0.5
-            y = 0.5
-        else:
-#            x = x
-#            y = y
-            next = False
-
-        return x,y,next
-
-
-def get_object_distance(marker):
-    if len(marker) > 0:
-        #finding distance from camera into image
-        focal_length, known_width = modul.finding_focal_length()
-        detected_marker = modul.detail_object(marker, "biggest_contour")
-        distance = modul.distance_to_camera(known_width, focal_length, detected_marker[1][0])
-
-        #print out the result
-        cv2.putText(modul.img, "Distance camera to object : ".format(distance), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,255,255), 2)
-
-        return distance
-
 def main():
     #initialize ros node
-    rospy.init_node("Object Following", anonymous="True")
+    rospy.init_node("ObjectFollowing", anonymous="True")
     
     #wait for FCU connection
     drone.wait4connect()
@@ -77,17 +99,19 @@ def main():
 
     # Specify control loop rate. We recommend a low frequency to not over load the FCU with messages. Too many messages will cause the drone to be sluggish.
     rate = rospy.Rate(3)
+
     
     while True:
-        #start video and detect orange object there
-        marker = modul.finding_object()
+        #initialize detect_an_object class as an object   
+        objek = detect_an_object()
 
-        if len(marker) > 0:
+        if len(objek.contours) > 0:
             #get centroid object
-            next = get_centroid_object(marker)
+            next = objek.get_centroid_object()
 
             #show up the detected object in the frame
             cv2.imshow("Object Following", modul.img)
+            cv2.waitKey(1)
 
             #Ask drone to fly into the desire position
             drone.set_destination(next[0], next[1], altitude, 0)
